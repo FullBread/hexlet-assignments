9fe86bcbc2bc40a54ffe1f259e05ea770f460b11
@hexlet/cli <support@hexlet.io>
2023-05-10 22:22:26 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -9,27 +9,29 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
-    List<Integer> numbers;
-    @BeforeEach
-    void initIntegerList() {
-        this.numbers = new ArrayList<>();
-        this.numbers.add(25);
-        this.numbers.add(35);
-        this.numbers.add(15);
-        this.numbers.add(124);
-    }
+//    List<Integer> numbers;
+//    @BeforeEach
+//    void initIntegerList() {
+//        this.numbers = new ArrayList<>();
+//        this.numbers.add(25);
+//        this.numbers.add(35);
+//        this.numbers.add(15);
+//        this.numbers.add(124);
+//    }
     @Test
     void testTake() {
-        var actual1 = App.take(numbers, 2);
-        List<Integer> answer1 = new ArrayList(Arrays.asList(25, 35));
+        List<Integer> q1 = new ArrayList(Arrays.asList(1, 2, 3, 4));
+        var actual1 = App.take(q1, 2);
+        List<Integer> answer1 = new ArrayList(Arrays.asList(1, 2));
         assertThat(actual1).isEqualTo(answer1);
 
-        var actual2 = App.take(numbers, 4);
-        List<Integer> answer2 = new ArrayList(Arrays.asList(25, 35, 15, 124));
+        List<Integer> q2 = new ArrayList(Arrays.asList(7, 3, 10));
+        var actual2 = App.take(q2, 8);
+        List<Integer> answer2 = new ArrayList(Arrays.asList(7, 3, 10));
         assertThat(actual2).isEqualTo(answer2);
 
-        var actual = App.take(numbers, 0);
-        List<Integer> answer = new ArrayList(Arrays.asList());
-        assertThat(actual).isEqualTo(answer);
+//        var actual = App.take(numbers, 0);
+//        List<Integer> answer = new ArrayList(Arrays.asList());
+//        assertThat(actual).isEqualTo(answer);
     }
 }
